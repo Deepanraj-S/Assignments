99efaf10840f6efb6939f8bce97c7cfaca9c2e8e
Deepanraj-S <151744005+Deepanraj-S@users.noreply.github.com>
2023-11-22 17:48:05 +0530

    Add files via upload
    Day1

---
+++ Welcome.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+import week1.day2.Learnvariable;
+
+public class Welcome {
+	public static void main(String[] args) {
+		
+System.out.println("Welcome to Test Leaf");
+Learnvariable count=new Learnvariable();
+count.add(11, 12);
+}
+	
+
+}
